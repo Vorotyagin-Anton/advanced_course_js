5f0badee74d771686f63f835a50c50346deee452
Anton Vorotyagin <a.vorotyagin@yandex.ru>
2020-02-17 00:02:43 +0300

    homework01

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 const products = [
     {id: 1, title: 'Notebook', price: 20000},
     {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
+    {id: 3, title: 'Keyboard', price: 5000, img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MLA22LL?wid=100&hei=100&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1496944005839"},
     {id: 4, title: 'Gamepad', price: 4500},
 ];
 
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="by-btn">Добавить в корзину</button>
-            </div>`;
-};
+const renderProduct = (title, price, img = "http://dummyimage.com/100x100/4d494d/686a82.gif&text=placeholder+image") => `<div class="product-item">
+                                                                                                                            <div class="product-item__image" style="background-image: url(${img})"></div>
+                                                                                                                            <h3>${title}</h3>
+                                                                                                                            <p>${price}</p>
+                                                                                                                            <button class="by-btn">Добавить в корзину</button>
+                                                                                                                         </div>`;
 
 const renderProducts = (list) => {
-    const productList = list.map((item) => renderProduct(item.title, item.price));
-    document.querySelector('.products').innerHTML = productList;
+    list.forEach((item, index) => {document.querySelector('.products').insertAdjacentHTML("beforeend", list.map(item => renderProduct(item.title, item.price, item.img))[index])});
 };
 
 renderProducts(products);
