be9dfb030c8ec5bf0ac4888e2a373c0ba2e216c4
Anton Vorotyagin <a.vorotyagin@yandex.ru>
2020-02-24 14:46:02 +0300

    lesson2 base code

---
+++ lesson2/js/main.js
@@ -0,0 +1,74 @@
+class ProductItem {
+  constructor(product, img='https://placehold.it/200x150') {
+    this.title = product.title;
+    this.price = product.price;
+    this.id = product.id;
+    this.img = img;
+  }
+
+  render() {
+    return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} \u20bd</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+            </div>`
+  }
+}
+
+class ProductList {
+  constructor(container = '.products') {
+    this.container = container;
+    this.goods = [];
+    this.allProducts = [];
+    this._fetchProducts();
+    this.render();
+    console.log(this.goods);
+    console.log(this.allProducts);
+  }
+
+  _fetchProducts() {
+    this.goods = [
+      {id: 1, title: 'Notebook', price: 1000},
+      {id: 2, title: 'Mouse', price: 100},
+      {id: 3, title: 'Keyboard', price: 250},
+      {id: 4, title: 'Gamepad', price: 150},
+    ];
+  }
+
+  render() {
+    const block = document.querySelector(this.container);
+
+    for (let product of this.goods) {
+      const productObject = new ProductItem(product);
+      this.allProducts.push(productObject);
+      block.insertAdjacentHTML('beforeend', productObject.render());
+    }
+  }
+}
+
+new ProductList();
+
+// const products = [
+//   {id: 1, title: 'Notebook', price: 1000},
+//   {id: 2, title: 'Mouse', price: 100},
+//   {id: 3, title: 'Keyboard', price: 250},
+//   {id: 4, title: 'Gamepad', price: 150},
+// ];
+//
+// const renderProduct = (item, img='https://placehold.it/200x150') => `<div class="product-item">
+//                 <img src="${img}" alt="Some img">
+//                 <div class="desc">
+//                     <h3>${item.title}</h3>
+//                     <p>${item.price} \u20bd</p>
+//                     <button class="buy-btn">Купить</button>
+//                 </div>
+//             </div>`;
+//
+// const renderProducts = list => {
+//   document.querySelector('.products').insertAdjacentHTML('beforeend', list.map(item => renderProduct(item)).join(''));
+// };
+//
+// renderProducts(products);
